b64e3abd14bda2775b4e929e0008faa1800a38f3
kurniadikevin <kurniadikevin@gmail.com>
2022-08-10 20:13:51 +0700

    add barebone on home page

--- src/Home.js
+++ src/Home.js
@@ -1,10 +1,33 @@
 import Dashboard from './dashboard';
+import homeImage from './image/coffee-roaster.jpg';
+import {Link} from 'react-router-dom';
 
 const Home= ()=>{
   return (
     <div className="home">
               <Dashboard />
-        <h1>Home page</h1>
+              <div className='home-main'>
+                <div className='home-col1'>
+                      <h1>Home page</h1>
+                      <img src={homeImage} height={533} width={800}
+                      alt='home-img' className='home-img'></img>
+                </div>
+                <div className='home-col2'>
+                    <div className='home-text1'>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+                    Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+                    </div>
+                    <div className='home-text2'>Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum Lorem ipsum 
+                    </div>
+                    <div className='home-text3'> Shibuya, Japan</div>
+                    <div className='shop-link'>
+                      <nav>
+                      <Link to = '/cart'>See our Product</Link>
+                      </nav>
+                    </div>
+                </div>
+
+              </div>
+             
     </div>
   );
 }
--- src/dashboard.js
+++ src/dashboard.js
@@ -1,15 +1,23 @@
 import {Link, Outlet} from 'react-router-dom';
+import './index.css';
 
 const Dashboard = () => {
     return(
-        <div>
-             <nav>
-        <Link to = '/'>Home</Link> |{'  '}
-        <Link to = '/shop'>Shop</Link> |{'  '}
-        <Link to = '/cart'>Cart</Link> |{'  '}
-        < Outlet />
-      </nav>    
-        </div>
+      <div className='dashboard'>
+            <div className='header'>
+                <div className='store-name'>
+                  S Coffee
+                </div>
+            </div>
+            <div>
+                    <nav>
+                <Link to = '/'>Home</Link> |{'  '}
+                <Link to = '/shop'>Shop</Link> |{'  '}
+                <Link to = '/cart'>Cart</Link> |{'  '}
+                < Outlet />
+              </nav>    
+            </div>
+      </div>
     )
 }
 
